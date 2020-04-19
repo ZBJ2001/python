66b8773f02e3dfa41ec17330de1784c41ba6c53c
zbj2001 <46210880@qq.com>
2020-04-19 21:47:05 +0800

    1

--- 面向对象/demo15-4.py
+++ 面向对象/demo15-4.py
@@ -4,8 +4,9 @@
     1,隐藏属性不被外界随意修改
     2,可以通过函数完成.
 '''
+
+
 class Student:
-    __age = 18
 
     def __init__(self, name, age):
         self.__name = name
@@ -17,13 +18,24 @@ class Student:
 
     # 定义公有的set,get方法。
     def set_age(self, age):
-        if age>0 and age<= 100:
+        if age > 0 and age <= 100:
             self.__age = age
         else:
             print('年龄不在规定的范围内！')
 
     def get_age(self):
         return self.__age
+
+    @property  # 属性装饰器。 就可以用self.name
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) < 10:
+            __name = name
+        else:
+            print('name is too long')
 
 
 stu1 = Student('zbj', 45)
@@ -32,17 +44,15 @@ stu1.set_age(30)
 stu1.set_age(130)
 print(stu1)
 
-
-#显示类的自带的属性
+# 显示类的自带的属性
 print(dir(Student))
-#显示对象的自带属性 将私有的 变成_Student__name显示。
+# 显示对象的自带属性 将私有的 变成_Student__name显示。
 print(dir(stu1))
 print(stu1._Student__age)
 
 print(stu1.__dir__())
 
-
-#突然想到判断是不是中文的问题。这里有个知识点是\u
+# 突然想到判断是不是中文的问题。这里有个知识点是\u
 print(len('zbj'))
 print(len('张宝军'))
 str1 = 'a'
